37b318c25836c8a74b79839f731f5349fd3d54b3
nm2002222 <107249392+nm2002222@users.noreply.github.com>
2022-06-23 09:15:00 +0200

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,38 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="register-players.php">
+    <div class="welcome">
+        <h1>Challenge Game on!</h1>
+        <h2>NAMEN IN DE VAKEN</h2>
+
+        <div class="player-name">
+            <label for="player-x">Speler 1 (X)</label>
+            <input type="text" id="player-x" name="player-x" required />
+        </div>
+
+        <div class="player-name">
+            <label for="player-o">Speler 2 (O)</label>
+            <input type="text" id="player-o" name="player-o" required />
+        </div>
+
+        <button type="submit">Start</button>
+    </div>
+</form>
+</body>
+</html>
+<?php
+require_once "./templates/header.php";
+?>
+
+
+
+
